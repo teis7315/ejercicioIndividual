c512dd13b7989bdfefb2c07c8ad303f9a121bb70
Gabriel Villar Fernández <a25gabrielvf@teis.intranet>
2025-11-05 15:28:33 +0100

    login added

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Versión inicial del proyecto.");
+        System.out.println("Funcionalidad de login implementada.");
     }
 }
